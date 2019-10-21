igor-benevides/python

expose MSR registers

libvmi/libvmi.py
@@ -9,6 +9,9 @@ INIT_DOMAINNAME = lib.VMI_INIT_DOMAINNAME
 INIT_DOMAINID = lib.VMI_INIT_DOMAINID
 INIT_EVENTS = lib.VMI_INIT_EVENTS
 
+MSR_ANY = lib.MSR_ANY
+MSR_ALL = lib.MSR_ALL
+
 
 class LibvmiError(Exception):
     pass
@@ -45,6 +48,49 @@ class X86Reg(Enum):
     DR3 = lib.DR3
     DR6 = lib.DR6
     DR7 = lib.DR7
+
+
+class MSR(Enum):
+    FLAGS = lib.MSR_FLAGS
+    LSTAR = lib.MSR_LSTAR
+    CSTAR = lib.MSR_CSTAR
+    SYSCALL_MASK = lib.MSR_SYSCALL_MASK
+    EFER = lib.MSR_EFER
+    TSC_AUX = lib.MSR_TSC_AUX
+    STAR = lib.MSR_STAR
+    SHADOW_GS_BASE = lib.MSR_SHADOW_GS_BASE
+    MTRRfix64K_00000 = lib.MSR_MTRRfix64K_00000
+    MTRRfix16K_80000 = lib.MSR_MTRRfix16K_80000
+    MTRRfix16K_A0000 = lib.MSR_MTRRfix16K_A0000
+    MTRRfix4K_C0000 = lib.MSR_MTRRfix4K_C0000
+    MTRRfix4K_C8000 = lib.MSR_MTRRfix4K_C8000
+    MTRRfix4K_D0000 = lib.MSR_MTRRfix4K_D0000
+    MTRRfix4K_D8000 = lib.MSR_MTRRfix4K_D8000
+    MTRRfix4K_E0000 = lib.MSR_MTRRfix4K_E0000
+    MTRRfix4K_E8000 = lib.MSR_MTRRfix4K_E8000
+    MTRRfix4K_F0000 = lib.MSR_MTRRfix4K_E8000
+    MTRRfix4K_F8000 = lib.MSR_MTRRfix4K_F8000
+    MTRRdefType = lib.MSR_MTRRdefType
+    IA32_MC0_CTL = lib.MSR_IA32_MC0_CTL
+    IA32_MC0_STATUS = lib.MSR_IA32_MC0_STATUS
+    IA32_MC0_ADDR = lib.MSR_IA32_MC0_ADDR
+    IA32_MC0_MISC = lib.MSR_IA32_MC0_MISC
+    IA32_MC1_CTL = lib.MSR_IA32_MC1_CTL
+    IA32_MC0_CTL2 = lib.MSR_IA32_MC0_CTL2
+    AMD_PATCHLEVEL = lib.MSR_AMD_PATCHLEVEL
+    AMD64_TSC_RATIO = lib.MSR_AMD64_TSC_RATIO
+    IA32_P5_MC_ADDR = lib.MSR_IA32_P5_MC_ADDR
+    IA32_P5_MC_TYPE = lib.MSR_IA32_P5_MC_TYPE
+    IA32_TSC = lib.MSR_IA32_TSC
+    IA32_PLATFORM_ID = lib.MSR_IA32_PLATFORM_ID
+    IA32_EBL_CR_POWERON = lib.MSR_IA32_EBL_CR_POWERON
+    IA32_EBC_FREQUENCY_ID = lib.MSR_IA32_EBC_FREQUENCY_ID
+    IA32_FEATURE_CONTROL = lib.MSR_IA32_FEATURE_CONTROL
+    IA32_SYSENTER_CS = lib.MSR_IA32_SYSENTER_CS
+    IA32_SYSENTER_ESP = lib.MSR_IA32_SYSENTER_ESP
+    IA32_SYSENTER_EIP = lib.MSR_IA32_SYSENTER_EIP
+    IA32_MISC_ENABLE = lib.MSR_IA32_MISC_ENABLE
+    HYPERVISOR = lib.MSR_HYPERVISOR
 
 
 class Registers:

msgpo/python-1

libvmi/libvmi.py
@@ -47,6 +47,51 @@ class X86Reg(Enum):
     DR7 = lib.DR7
 
 
+class MSR(Enum):
+    ANY = lib.MSR_ANY
+    ALL = lib.MSR_ALL
+    FLAGS = lib.MSR_FLAGS
+    LSTAR = lib.MSR_LSTAR
+    CSTAR = lib.MSR_CSTAR
+    SYSCALL_MASK = lib.MSR_SYSCALL_MASK
+    EFER = lib.MSR_EFER
+    TSC_AUX = lib.MSR_TSC_AUX
+    STAR = lib.MSR_STAR
+    SHADOW_GS_BASE = lib.MSR_SHADOW_GS_BASE
+    MTRRfix64K_00000 = lib.MSR_MTRRfix64K_00000
+    MTRRfix16K_80000 = lib.MSR_MTRRfix16K_80000
+    MTRRfix16K_A0000 = lib.MSR_MTRRfix16K_A0000
+    MTRRfix4K_C0000 = lib.MSR_MTRRfix4K_C0000
+    MTRRfix4K_C8000 = lib.MSR_MTRRfix4K_C8000
+    MTRRfix4K_D0000 = lib.MSR_MTRRfix4K_D0000
+    MTRRfix4K_D8000 = lib.MSR_MTRRfix4K_D8000
+    MTRRfix4K_E0000 = lib.MSR_MTRRfix4K_E0000
+    MTRRfix4K_E8000 = lib.MSR_MTRRfix4K_E8000
+    MTRRfix4K_F0000 = lib.MSR_MTRRfix4K_E8000
+    MTRRfix4K_F8000 = lib.MSR_MTRRfix4K_F8000
+    MTRRdefType = lib.MSR_MTRRdefType
+    IA32_MC0_CTL = lib.MSR_IA32_MC0_CTL
+    IA32_MC0_STATUS = lib.MSR_IA32_MC0_STATUS
+    IA32_MC0_ADDR = lib.MSR_IA32_MC0_ADDR
+    IA32_MC0_MISC = lib.MSR_IA32_MC0_MISC
+    IA32_MC1_CTL = lib.MSR_IA32_MC1_CTL
+    IA32_MC0_CTL2 = lib.MSR_IA32_MC0_CTL2
+    AMD_PATCHLEVEL = lib.MSR_AMD_PATCHLEVEL
+    AMD64_TSC_RATIO = lib.MSR_AMD64_TSC_RATIO
+    IA32_P5_MC_ADDR = lib.MSR_IA32_P5_MC_ADDR
+    IA32_P5_MC_TYPE = lib.MSR_IA32_P5_MC_TYPE
+    IA32_TSC = lib.MSR_IA32_TSC
+    IA32_PLATFORM_ID = lib.MSR_IA32_PLATFORM_ID
+    IA32_EBL_CR_POWERON = lib.MSR_IA32_EBL_CR_POWERON
+    IA32_EBC_FREQUENCY_ID = lib.MSR_IA32_EBC_FREQUENCY_ID
+    IA32_FEATURE_CONTROL = lib.MSR_IA32_FEATURE_CONTROL
+    IA32_SYSENTER_CS = lib.MSR_IA32_SYSENTER_CS
+    IA32_SYSENTER_ESP = lib.MSR_IA32_SYSENTER_ESP
+    IA32_SYSENTER_EIP = lib.MSR_IA32_SYSENTER_EIP
+    IA32_MISC_ENABLE = lib.MSR_IA32_MISC_ENABLE
+    HYPERVISOR = lib.MSR_HYPERVISOR
+
+
 class Registers:
     """
     This class acts as a wrapper on top of
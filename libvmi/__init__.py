c34a664060a34f552b0a920e8f9180c7de206ad2
Mathieu Tarral <mathieu.tarral@protonmail.com>
2019-10-14 01:00:42 +0200

    expose MSR registers

--- libvmi/__init__.py
+++ libvmi/__init__.py
@@ -2,6 +2,6 @@ from __future__ import absolute_import
 # public interface
 from .libvmi import INIT_DOMAINNAME, INIT_DOMAINID, INIT_EVENTS
 from .libvmi import (Libvmi, LibvmiError, VMIConfig, VMIMode, AccessContext,
-                     TranslateMechanism, X86Reg, Registers)
+                     TranslateMechanism, X86Reg, MSR, Registers)
 from .libvmi import VMIStatus, LibvmiInitError, PageMode
 from .libvmi import VMIArch, VMIOS, VMIWinVer
